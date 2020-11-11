0502568e245145bd82c24e6e0710c61be24fe707
fjaskolka <filipjask512@gmail.com>
2020-11-11 22:14:13 +0000

    v5

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -4,48 +4,39 @@
 
 
 class ResourceManager {
-
   private:
     Resource* m_resource;
-
   public:
-    ResourceManager(); // konstruktor
-    ~ResourceManager(); // destruktor
-    ResourceManager(const ResourceManager& a); // konstruktor kopiujący
-    ResourceManager& operator=(const ResourceManager& a); // kopiujacy operator przypisania
-    ResourceManager(ResourceManager&& a); // kontruktor przenoszacy
-
-    double get() { return m_resource->get(); }
+    ResourceManager() // utwórz obiekt resource
+    {
+      m_resource = new Resource();
+      m_resource = new Resource;
+    }
+    ~ResourceManager() // zwolnij obiekt resource
+    {
+      delete m_resource;
+    }
+    ResourceManager(const ResourceManager& a) // konstruktor kopiujący
+    {
+      m_resource = new Resource(*(a.m_resource));
+    }
+    ResourceManager& operator=(ResourceManager&& a) // operator przypisania
+    {
+      if(&a == this)
+      {
+        return *this;
+      }
+      delete m_resource;
+      m_resource = new Resource(*(a.m_resource));
+      return *this;
+    }
+    ResourceManager(ResourceManager&& a) // konstruktor przenoszący
+    {
+      m_resource = a.m_resource;
+      a.m_resource = nullptr;   
+    }
+    double get()
+    {
+      return m_resource->get();
+    }
 };
-
-ResourceManager::ResourceManager()// konstruktor
-{
-  m_resource = new Resource;
-}
-
-ResourceManager::~ResourceManager()// destruktor
-{
-  delete m_resource;
-}
-
-ResourceManager::ResourceManager(const ResourceManager& a)// konstruktor kopiujący
-{
-  m_resource = new Resource(*(a.m_resource));
-}
-
-ResourceManager& ResourceManager::operator=(const ResourceManager& a); // kopiujacy operator przypisania
-{
-  if(&a == this)
-  {
-    return *this;
-  }
-  delete m_resource;
-  m_resource = new Resource(*(a.m_resource));
-  return *this;
-}
-
-ResourceManager::ResourceManager(ResourceManager&& a) // konstruktor przenoszący
-{
-  m_resource = a.m_resource;
-  a.m_resource = nullptr;
-}
